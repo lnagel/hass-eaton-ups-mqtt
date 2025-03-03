7f2d72d7af83cac2e2597bc90b9f1918362a38fb
Lenno Nagel <lenno@namespace.ee>
2025-03-03 12:21:48 +0200

    refactor: Simplify manager identification data retrieval in diagnostics

--- custom_components/eaton_ups/diagnostics.py
+++ custom_components/eaton_ups/diagnostics.py
@@ -19,30 +19,8 @@ async def async_get_config_entry_diagnostics(
     coordinator: EatonUPSDataUpdateCoordinator = config_entry.runtime_data.coordinator
 
     # Get manager identification data from coordinator
-    # The path matches the MQTT topic structure from the documentation
-    identification_topic = "mbdetnrs/1.0/managers/1/identification"
-    manager_data = coordinator.data.get(identification_topic, {})
+    manager_identification = coordinator.data.get("managers/1/identification", {})
 
     return {
-        "manager_identification": {
-            "firmware_version": manager_data.get("firmwareVersion"),
-            "physical_name": manager_data.get("physicalName"),
-            "uuid": manager_data.get("uuid"),
-            "vendor": manager_data.get("vendor"),
-            "product": manager_data.get("product"),
-            "serial_number": manager_data.get("serialNumber"),
-            "type": manager_data.get("type"),
-            "part_number": manager_data.get("partNumber"),
-            "hw_version": manager_data.get("hwVersion"),
-            "name": manager_data.get("name"),
-            "contact": manager_data.get("contact"),
-            "location": manager_data.get("location"),
-            "firmware_installation_date": manager_data.get("firmwareInstallationDate"),
-            "firmware_activation_date": manager_data.get("firmwareActivationDate"),
-            "firmware_date": manager_data.get("firmwareDate"),
-            "firmware_sha": manager_data.get("firmwareSha"),
-            "bootloader_version": manager_data.get("bootloaderVersion"),
-            "manufacturer": manager_data.get("manufacturer"),
-            "mac_address": manager_data.get("macAddress"),
-        },
+        "manager_identification": manager_identification,
     }
